469d1f0cf9019095fac73399fa9a949f5c076e6c
tikoriski99 <56874072+tikoriski99@users.noreply.github.com>
2019-11-14 11:27:26 +0700

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package looping;
+
+/**
+ *
+ * @author User
+ */
+public class Looping {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    
+    System.out.println ("=====for=====");
+    int a=0;
+    for(a=0;a<5;a++){
+    System.out.println("Nilai a:"+a);
+    }
+    
+    System.out.println ("=====While=====");
+    int i=0;
+    while (i<5){
+    System.out.println("riski");
+    i++;}
+    
+ 
+   System.out.println  ("=====do=====");
+   int b=0;   
+   do{
+       System.out.println(b);
+       b++;
+   }
+   while(b<5);
+   
+          
+   }
+    }
+    
